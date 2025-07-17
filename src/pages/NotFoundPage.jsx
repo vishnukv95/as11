3589ac8539ee565fe629c4dfa49b985f532a44e0
vishnukv95 <megawat95@gmail.com>
2025-07-17 19:16:32 +0530

    just some fixes

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
+    <div className='flex place-content-center mt-52 p-7'>
+      <h2 className='text-red-500 text-4xl'>
+        The page you are trying to access does not exist
+      </h2>
+    </div>
   )
 }
 
